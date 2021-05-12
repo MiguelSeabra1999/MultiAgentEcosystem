b1212760b2efca50b32493ec51f0017332962031
Inês Margarida Caetano Correia <32440369+inesmargarida@users.noreply.github.com>
2021-05-12 23:37:29 +0100

    Revert "commit desperate"
    This reverts commit 7a7d4b010b105dc46539b0b6ac403b7d77a67a85.

--- Assets/Scripts/Agent/State/State.cs
+++ Assets/Scripts/Agent/State/State.cs
@@ -8,22 +8,26 @@ using UnityEngine;
 public class State : MonoBehaviour
 {
     public float consistentHungerDrainRate = 0.01f;
+    public float consistentPeaceDrainRate = 0.01f;
     public float hp;
     public float hunger = 100;
+    public float peace = 100;
     private Genome genome;
     private Animator animator;
+    private Feel feel;
     private RendererFXInterface rendererFXInterface;
 
     private void Awake() {
         genome = GetComponent<Genome>();
+        feel = GetComponent<Feel>();
         rendererFXInterface = GetComponent<RendererFXInterface>();
         animator = GetComponentInChildren<Animator>();
         hp = genome.vitality;
-        
     }
 
     private void FixedUpdate() {
         hunger -= consistentHungerDrainRate;
+        peace -= consistentPeaceDrainRate*genome.angryDamage;
 
         if(hunger <= 0)
         {
@@ -31,10 +35,18 @@ public class State : MonoBehaviour
             hp -= genome.starvingDamage;
         }
 
-        if(hp <= 0)
+        if (hp <= 0)
         {
             StarveToDeath();
         }
+
+        if (peace <= 0)
+        {
+            peace = 0;
+            if (feel.feelingAgent)
+                Attack();
+        }
+
     }
 
     private void StarveToDeath()
@@ -43,6 +55,22 @@ public class State : MonoBehaviour
         rendererFXInterface.StartFadeOut();
         GetComponent<MoveActuator>().SetMovement(Vector2.zero);
         Invoke("DestroySelf", 2);  
+
+    }
+
+    private void Attack()
+    {
+        animator.SetTrigger("Attack");   
+
+    }
+
+    //delete starve to death
+    public void Die()
+    {
+        animator.SetTrigger("Die");
+        rendererFXInterface.StartFadeOut();
+        GetComponent<MoveActuator>().SetMovement(Vector2.zero);
+        Invoke("DestroySelf", 2);
 
     }
 
@@ -56,5 +84,12 @@ public class State : MonoBehaviour
         hunger += nutricionalValue;
         if(hunger > 100)
             hunger = 100;
+    }
+
+    public void HealPeace(float attackValue)
+    {
+        peace += attackValue;
+        if (peace > 100)
+            peace = 100;
     }
 }
